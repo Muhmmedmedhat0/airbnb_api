a886101a1fab0ae45d95310128737ee4403ba37a
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-09 00:07:03 +0200

    validaitin on login and signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,19 @@ const User = require('../models/User');
 const bycript = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = global.process.env.JWT_SECRET;
+const { validationResult } = require('express-validator');
 
 // signup controller for authentication
 exports.signup = async (req, res, next) => {
+  // adding validation to the request body
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422).json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array(),
+      });
+  }
   // check if user already exists in database
   const { email } = req.body;
   const user = await User.findOne({ email });
@@ -61,7 +71,7 @@ exports.login = async (req, res, next) => {
         { expiresIn: '5h' }
       );
 
-      const { password, isAdmin ,...otherDetails } = loadedUser._doc;
+      const { password, isAdmin, ...otherDetails } = loadedUser._doc;
       // send user details and token to client side
       // res.status(200).json({
       //   message: `Welcome ${loadedUser.name}!`,
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 const authController = require('../controllers/auth');
 
 // signup
-router.post('/signup', authController.signup);
+router.post(
+  '/signup',
+  [
+    body('name')
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage('Name must be at least 3 characters long.'),
+    body('email')
+      .isEmail()
+      .trim()
+      .normalizeEmail()
+      .withMessage('Invalid email'),
+    body('password')
+      .trim()
+      .isStrongPassword()
+      .withMessage(
+        `Password must be at least 8 characters long 
+        and contain at least one number, one uppercase 
+        letter and one lowercase letter`
+      ),
+  ],
+  authController.signup
+);
 
 // login
-router.post('/login', authController.login);
+router.post(
+  '/login',
+  [body('email').isEmail().trim().normalizeEmail(),
+    body('password').trim()],
+  authController.login
+);
 module.exports = router;
