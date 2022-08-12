f14a172fa059ebaf4dff27a7d0fda4a4d0af5fe0
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-12 02:45:14 +0200

    updated db

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/png' ||
     file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/webp'
   ) {
     // valid file
     cb(null, true);
